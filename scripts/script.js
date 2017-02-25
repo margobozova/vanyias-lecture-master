b80d545fc629227f6acd573585c357db2a9d8363
Margo Bozova <you@example.com>
2017-02-25 14:32:02 +0200

    Change function post, change server side

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,20 @@ const gallery = document.getElementById('gallery'),
     friendsList = document.getElementById('friends'),
     button = document.querySelector('button'),
     input = document.querySelector('input'),
-    replies = document.querySelector('.replies')
+    replies = document.querySelector('.replies'),
+    photoInput = document.getElementById('photo-input');
+
+    photoInput.addEventListener('change', (e) => {
+    let file = e.target.files[0];
+    let types = ['image/jpeg', 'image/gif', 'image/png'];
+
+    if (!file || !types.includes(file.type)) {
+        e.target.value = null;
+        throw new TypeError('Wrong type of file');
+    }
+
+    uploadPhoto(file);
+});
 
 // Markup elements
 button.addEventListener('click', addReply);
@@ -31,8 +44,28 @@ function get(url, callback) {
         (xhr.status == 200 && xhr.readyState == 4) && callback(JSON.parse(xhr.responseText))
     };
 
+    xhr.onerror = (e) => {
+        onError(e);
+    };
     xhr.open('GET', url, true);
     xhr.send(null);
+}
+
+function onError (e) {
+    throw new Error(e.message);
+}
+function uploadPhoto(photo) {
+    let container = {
+        url:'/photos',
+        resolve: onSucces,
+        reject: onError,
+        data: photo
+    };
+
+    function onSucces (response) {
+        console.log(response);
+    }
+    post(container);
 }
 
 // Elements rendering
@@ -47,8 +80,9 @@ function drawPhotos(photos) {
         photo.appendChild(img);
         gallery.appendChild(photo);
     });
+    return gallery;
 }
-get('/photos', drawPhotos);
+get('/photos', drawPhotos, onError);
 
 function showProfile(profile) {
     profile = profile['results'][0];
@@ -69,7 +103,22 @@ function showFriendsList(friends) {
         img.src = friend.picture['large'];
 
         friendPhoto.appendChild(img);
-        friendsList.appendChild(friendPhoto)
+        friendsList.appendChild(friendPhoto);
     })
+}
+function post (params) {
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData();
+
+    formData.append('file' , params.data);
+    xhr.onreadystatechange = function () {
+        if (xhr.status == 200 && xhr.readyState == 4) {
+            params.resolve(JSON.parse(xhr.responseText));
+        }
+    };
+    
+    
+    xhr.open('POST', params.url, true);
+    xhr.send(formData);
 }
 get('https://randomuser.me/api/?results=15', showFriendsList);
--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 let data = require('./data.js');
 const express = require('express');
 
+const path = require('path');
+const fs = require('fs');
+const formidable = require('formidable');
+
 const  app = express();
+
 
 app.use(express.static(__dirname));
 app.listen(8080);
@@ -15,5 +20,27 @@ app.get('/', (request, response) => {
 app.get('/photos', (request, response) => {
    response.send(JSON.stringify((data.photos)))
 });
+app.post('/photos', (request, response) => {
+  let form = new formidable.IncomingForm();
 
+  let name = '';
+
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  form.on('file', (field, file) => {
+    name = file.name;
+
+    fs.rename(file.path, path.join(form.uploadDir, name))
+  });
+
+  form.on('end', () => {
+    let container = {
+      id: Date.now()*Math.random(),
+      url: `/uploads/${name}`
+    };
+    response.send(JSON.stringify(container));
+  });
+  
+  form.parse(request);
+});
 console.log('Server running on port 8080');
